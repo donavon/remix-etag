e7e0812c6522e9c521a03f5ac0d5336a008af800
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-06-06 16:19:42 +0900

    refactor: etag generator

--- src/eTag.ts
+++ src/eTag.ts
@@ -1,30 +1,28 @@
-async function entityTag(entity: string): Promise<string> {
+const entityTag = async (entity: string): Promise<string> => {
   if (entity.length === 0) {
-    // fast-path empty
-    return '"0-2jmj7l5rSw0yVb/vlWAYkK/YBwk"';
+    return '"0a4d55a8d778e5022fab701977c5d840bbc486d0"';
   }
-  const target = new TextEncoder().encode(entity);
-  const hash = await crypto.subtle.digest('SHA-1', target).then(hashBuffer => {
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
-      .map(function(b) {
-        return b.toString(16).padStart(2, '0');
-      })
-      .join('');
-    return hashHex;
-  });
 
-  return `"${hash}"`;
-}
+  const target = new TextEncoder().encode(entity);
+  const hashBuffer = await crypto.subtle.digest('SHA-1', target);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map(function(b) {
+      return b.toString(16).padStart(2, '0');
+    })
+    .join('');
+
+  return `"${hashHex}"`;
+};
 
 /**
  * Create a simple ETag.
  */
 
-export async function createEtag(
+export const createEtag = async (
   entity: string,
   options?: { weak: boolean }
-): Promise<string> {
+): Promise<string> => {
   if (entity == null) {
     throw new TypeError('argument entity is required');
   }
@@ -33,4 +31,4 @@ export async function createEtag(
   const tag = await entityTag(entity);
 
   return options?.weak ? `W/${tag}` : tag;
-}
+};
